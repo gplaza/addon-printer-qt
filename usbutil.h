927db716894db98823e586a401c92ef798a41cdf
Administrator <gilles.grandguillaume@gmail.com>
2017-11-25 21:23:03 -0300

    Add function dev_control

--- usbutil.h
+++ usbutil.h
@@ -6,9 +6,13 @@
 #define VENDOR_ID   1d90
 #define PRODUCT_ID  2060
 #define PORT_OUT    0x02
-#define PORT_IN     0x82
+#define PORT_IN     0x81
 
-#define USB_TIMEOUT	100	    // 1 secconds
+#define USBLP_REQ_GET_ID      0x00
+#define USBLP_REQ_GET_STATUS  0x01
+#define USBLP_REQ_RESET       0x02
+
+#define USB_TIMEOUT	100	    // 1 sec
 
 class UsbUtil
 {
@@ -16,6 +20,7 @@ public:
     UsbUtil();
     int dev_find(QString vendor_id, QString product_id, libusb_device_handle **handle);
     int dev_write(libusb_device_handle *device_handle, QString data, int out_len);
+    int dev_control(libusb_device_handle *device_handle, unsigned char* _udata, uint8_t bRequest = USBLP_REQ_GET_STATUS, int out_len = 1);
     int dev_open(libusb_device_handle *device_handle);
     int dev_close(libusb_device_handle *device_handle);
 };
